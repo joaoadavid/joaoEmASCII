8cff33c107fdc3a8ac968afe3044b1ac241ba3ff
joao.david <joao.antoniodavid@hotmail.com>
2023-03-03 20:05:56 -0300

    Initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,175 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        char[][] arrA = letraA();
-        escrever(arrA);
-
-        char[][] arrB = letraB();
-        escrever(arrB);
-
-        char[][] arrC = letraC();
-        escrever(arrC);
-
-        char[][] arrD = letraD();
-        escrever(arrD);
-
-        char[][] arrE = letraE();
-        escrever(arrE);
-
-        char[][] arrF = letraF();
-        escrever(arrF);
-
-        char[][] arrG = letraG();
-        escrever(arrG);
-
-        char[][] arrH = letraH();
-        escrever(arrH);
-
-        char[][] arrI = letraI();
-        escrever(arrI);
-
         char[][] arrJ = letraJ();
         escrever(arrJ);
-
-        char[][] arrK = letraK();
-        escrever(arrK);
-
-        char[][] arrL = letraL();
-        escrever(arrL);
-
-        char[][] arrM = letraM();
-        escrever(arrM);
-
-        char[][] arrN = letraN();
-        escrever(arrN);
 
         char[][] arrO = letraO();
         escrever(arrO);
 
-        char[][] arrP = letraP();
-        escrever(arrP);
+        char[][] arrA = letraA();
+        escrever(arrA);
 
-        char[][] arrQ = letraQ();
-        escrever(arrQ);
-
-        char[][] arrR = letraR();
-        escrever(arrR);
-
-        char[][] arrS = letraS();
-        escrever(arrS);
-
-        char[][] arrT = letraT();
-        escrever(arrT);
-
-        char[][] arrU = letraU();
-        escrever(arrU);
-
-        char[][] arrV = letraV();
-        escrever(arrV);
-
-        char[][] arrW = letraW();
-        escrever(arrW);
-
-        char[][] arrX = letraX();
-        escrever(arrX);
-
-        char[][] arrY = letraY();
-        escrever(arrY);
-
-        char[][] arrZ = letraZ();
-        escrever(arrZ);
-    }
-
-    public static char[][] letraA() {
-        char[][] arr = {
-                {'A', 'A', 'A', 'A', 'A'},
-                {'A', ' ', ' ', ' ', 'A'},
-                {'A', 'A', 'A', 'A', 'A'},
-                {'A', ' ', ' ', ' ', 'A'},
-                {'A', ' ', ' ', ' ', 'A'}
-        };
-        return arr;
-    }
-
-    public static char[][] letraB() {
-        char[][] arr = {
-                {'B', 'B', 'B', 'B', ' '},
-                {'B', ' ', ' ', ' ', 'B'},
-                {'B', 'B', 'B', 'B', ' '},
-                {'B', ' ', ' ', ' ', 'B'},
-                {'B', 'B', 'B', 'B', ' '}
-        };
-        return arr;
-    }
-
-    public static char[][] letraC() {
-        char[][] arr = {
-                {' ', ' ', 'C', 'C', 'C'},
-                {' ', 'C', ' ', ' ', ' '},
-                {'C', ' ', ' ', ' ', ' '},
-                {' ', 'C', ' ', ' ', ' '},
-                {' ', ' ', 'C', 'C', 'C'}
-        };
-        return arr;
-    }
-    public static char[][] letraD() {
-        char[][] arr = {
-                {'D','D', 'D', 'D', 'D'},
-                {'D',' ', ' ', ' ',  'D'},
-                {'D', ' ', ' ', ' ', 'D'},
-                {'D', ' ', ' ',    'D', ' '},
-                {'D', 'D', 'D', 'D', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraE() {
-        char[][] arr = {
-                {'E', 'E', 'E', 'E', 'E'},
-                {' ', ' ', ' ', ' ', ' '},
-                {'E', 'E', 'E', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' '},
-                {'E', 'E', 'E', 'E', 'E'}
-        };
-        return arr;
-    }
-    public static char[][] letraF() {
-        char[][] arr = {
-                {'F', 'F', 'F', 'F', 'F'},
-                {'F', ' ', ' ', ' ', ' '},
-                {'F', 'F', 'F', ' ', ' '},
-                {'F', ' ', ' ', ' ', ' '},
-                {'F', ' ', ' ', ' ', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraG() {
-        char[][] arr = {
-                {'G', 'G', 'G', 'G', 'G'},
-                {'G', ' ', ' ', ' ', ' '},
-                {'G', ' ', 'G', 'G', 'G'},
-                {'G', ' ', ' ', ' ', 'G'},
-                {'G', 'G', 'G', 'G', 'G'}
-        };
-        return arr;
-    }
-    public static char[][] letraH() {
-        char[][] arr = {
-                {'H', ' ', ' ', ' ', 'H'},
-                {'H', ' ', ' ', ' ', 'H'},
-                {'H', 'H', 'H', 'H', 'H'},
-                {'H', ' ', ' ', ' ', 'H'},
-                {'H', ' ', ' ', ' ', 'H'}
-        };
-        return arr;
-    }
-    public static char[][] letraI() {
-        char[][] arr = {
-                {' ', 'I', 'I', 'I', ' '},
-                {' ', ' ', 'I', ' ', ' '},
-                {' ', ' ', 'I', ' ', ' '},
-                {' ', ' ', 'I', ' ', ' '},
-                {' ', 'I', 'I', 'I', ' '}
-        };
-        return arr;
+        char[][] arrOO = letraOO();
+        escrever(arrOO);
     }
     public static char[][] letraJ() {
         char[][] arr = {
@@ -178,46 +19,6 @@ public class HelloWorld {
                 {' ', ' ', 'J', ' ', ' '},
                 {'J', ' ', 'J', ' ', ' '},
                 {'J', 'J', 'J', ' ', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraK() {
-        char[][] arr = {
-                {'K', ' ', ' ','K', ' '},
-                {'K', ' ', 'K', ' ', ' '},
-                {'K', 'K', ' ', ' ', ' '},
-                {'K', ' ','K', ' ', ' '},
-                {'K', ' ', ' ', 'K', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraL() {
-        char[][] arr = {
-                {'L', ' ', ' ', ' ', ' '},
-                {'L', ' ', ' ', ' ', ' '},
-                {'L', ' ', ' ', ' ', ' '},
-                {'L', ' ', ' ', ' ', ' '},
-                {'L','L','L', ' ', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraM() {
-        char[][] arr = {
-                {'M', ' ', ' ',' ','M'},
-                {'M','M', ' ','M', 'M'},
-                {'M', ' ','M', ' ','M'},
-                {'M', ' ', ' ',' ','M'},
-                {'M', ' ', ' ',' ','M'}
-        };
-        return arr;
-    }
-    public static char[][] letraN() {
-        char[][] arr = {
-                {'N', ' ', ' ', ' ','N'},
-                {'N','N', ' ', ' ','N'},
-                {'N',' ','N', ' ','N'},
-                {'N',' ',' ','N',' '},
-                {'N', ' ',' ','N',' '}
         };
         return arr;
     }
@@ -231,116 +32,27 @@ public class HelloWorld {
         };
         return arr;
     }
-    public static char[][] letraP() {
+    public static char[][] letraA() {
         char[][] arr = {
-                {'P','P','P', ' ', ' '},
-                {'P',' ','P', ' ', ' '},
-                {'P','P','P', ' ', ' '},
-                {'P', ' ', ' ', ' ', ' '},
-                {'P', ' ', ' ', ' ', ' '}
+                {'A', 'A', 'A', 'A', 'A'},
+                {'A', ' ', ' ', ' ', 'A'},
+                {'A', 'A', 'A', 'A', 'A'},
+                {'A', ' ', ' ', ' ', 'A'},
+                {'A', ' ', ' ', ' ', 'A'}
         };
         return arr;
     }
-    public static char[][] letraQ() {
+    public static char[][] letraOO() {
         char[][] arr = {
-                {'Q','Q','Q','Q','Q'},
-                {'Q',' ', ' ', ' ','Q'},
-                {'Q', ' ',' ', ' ','Q'},
-                {'Q','Q','Q','Q', 'Q'},
-                {' ',' ',' ',' ','Q'}
+                {'O','O','O','O','O'},
+                {'O', ' ', ' ', ' ','O'},
+                {'O', ' ', ' ', ' ','O'},
+                {'O', ' ', ' ',' ','O'},
+                {'O', 'O','O','O','O'}
         };
         return arr;
     }
-    public static char[][] letraR() {
-        char[][] arr = {
-                {'R','R','R', ' ', ' '},
-                {'R', ' ','R', ' ', ' '},
-                {'R','R','R', ' ', ' '},
-                {'R', ' ','R', ' ', ' '},
-                {'R', ' ','R', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraS() {
-        char[][] arr = {
-                {'S', 'S', 'S', 'S', 'S'},
-                {'S', ' ', ' ', ' ', ' '},
-                {'S', 'S','S','S','S'},
-                {' ', ' ', ' ', ' ','S'},
-                {'S', 'S', 'S', 'S','S'}
-        };
-        return arr;
-    }
-    public static char[][] letraT() {
-        char[][] arr = {
-                {' ', 'T', 'T', 'T', ' '},
-                {' ', ' ', 'T', ' ', ' '},
-                {' ', ' ', 'T', ' ', ' '},
-                {' ', ' ', 'T', ' ', ' '},
-                {' ', ' ', 'T', ' ', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraU() {
-        char[][] arr = {
-                {'U', ' ', ' ' , 'U'},
-                {'U', ' ', ' ',  'U'},
-                {'U', ' ', ' ',  'U'},
-                {'U', ' ', ' ',  'U'},
-                {'U','U','U','U','U'}
-        };
-        return arr;
-    }
-    public static char[][] letraV() {
-        char[][] arr = {
-                {'V',  ' ',   'V'},
-                { 'V',' ',' ' ,'V',' '},
-                {' ','V',' ','V', },
-                {' ',  'V','V'},
-                {' ',' ','V',' ',' '}
-        };
-        return arr;
-    }
-    public static char[][] letraW() {
-        char[][] arr = {
-                {'W', ' ', ' ', ' ','W'},
-                {'W',' ', ' ', ' ', 'W'},
-                {'W', ' ','W', ' ', 'W'},
-                {'W','W', ' ','W',  'W'},
-                {'W', ' ', ' ', 'W',' '}
-        };
-        return arr;
-    }
-    public static char[][] letraX() {
-        char[][] arr = {
-                {' ', 'X',' ','X', ' '},
-                {' ',' ','X','X', ' '},
-                {' ', ' ','X',' ', ' '},
-                {' ',   'X', 'X',' ', ' '},
-                {' ', 'X', ' ', 'X', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraY() {
-        char[][] arr = {
-                {' ', 'Y',' ', 'Y', ' '},
-                {' ', ' ', 'Y',' ', ' '},
-                {' ', ' ', 'Y', ' ', ' '},
-                {' ', ' ', 'Y', ' ', ' '},
-                {' ', ' ', 'Y', ' ', ' '}
-        };
-        return arr;
-    }
-    public static char[][] letraZ() {
-        char[][] arr = {
-                {' ', 'Z',  'Z', 'Z','Z'},
-                {' ', ' ', ' ', 'Z', ' '},
-                {' ', ' ', 'Z', ' ', ' '},
-                {' ',   'Z', ' ', ' ', ' '},
-                {' ', 'Z', 'Z', 'Z', 'Z'}
-        };
-        return arr;
-    }
+
 
     public static void escrever(char[][] arr) {
         for (int i = 0; i < arr.length; i++) {
